fd8987035a5b712aa2292c0d93ae585fb786452b
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-26 02:23:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', function() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+
+    if(title==='' || author==='' || isbn==='') {
+        alert('Please fill in all fields');
+        return;
+    }
+
+
+    const table = document.getElementById('book-list');
+    const row = document.createElement('tr');
+    row.innerHTML=`
+        <td>${title}</td>
+        <td>${author}</td>
+        <td>${isbn}</td>
+        <td><button class="btn btn-danger btn-sm delete">X</button></td>
+        `;
+    table.appendChild(row);
+
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+});
+
+document.getElementById('book-list').addEventListener('click', function(e) {
+    if(e.target.classList.contains('delete')) {
+        e.target.parentElement.parentElement.remove();
+    }
+});
